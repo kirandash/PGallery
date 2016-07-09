3b58a3c6868624924942f3b4744ee82c08c1852b
kirandash <thekirankumardash@gmail.com>
2016-07-09 15:05:32 +0530

    Image Overlay Effect

--- js/script.js
+++ js/script.js
@@ -27,4 +27,42 @@ $(document).ready(function(e) {
 		//Stop Link Behavior
 		return false;
 	});
+	
+	//Mouseenter Overlay
+	$('ul#gallery li').on('mouseenter', function(){
+		//Get data attribute values
+		var title = $(this).data('title');
+		var desc = $(this).data('desc');
+		
+		//Validation
+		if(desc == null){
+			desc = 'Click to enlarge';
+		}
+		
+		if(title == null){
+			title = '';
+		}
+		
+		//Create overlay div
+		$(this).append('<div class="overlay"></div>');
+		
+		//Get the overlay div
+		var overlay = $(this).children('.overlay');
+		
+		//Add html to overlay
+		overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+		
+		//Fade in overlay
+		overlay.fadeIn(800);
+	});
+	
+	//Mouseleave Overlay
+	$('ul#gallery li').on('mouseleave', function(){		
+		//Get the overlay div
+		var overlay = $(this).children('.overlay');
+		
+		//Fade in overlay
+		overlay.fadeOut(500);
+		
+	});	
 });
